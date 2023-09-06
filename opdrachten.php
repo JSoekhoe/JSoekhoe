d6d172bd92b2a95587e0aa4b4a6c49e6cb2dd2c6
JSoekhoe <144214366+JSoekhoe@users.noreply.github.com>
2023-09-06 19:20:20 +0200

    Add files via upload

---
+++ opdrachten.php
@@ -0,0 +1,24 @@
+<?php
+/
+// Developer: Joerie Soekhoe
+
+/**
+ * Dit is de Assignment van College 1
+ * Deze Assignment bevat opdracht 1, 2, 3 en 4
+ */
+
+ $firstName = 'Joerie';
+ $lastName = 'Soekhoe';
+ $dateOfbirth = date('24-11-2001');
+ $age = 21;
+ $birthPlace = 'Enschede';
+ $email = 's1201693@student.windesheim.nl'; 
+ $mobileNumber = '+31641462290';
+ $hobbies = "Basketbal, Drummen, Piano spelen en Schoenen verkopen";
+ $favoriteFood = 'Tjauwmin';
+ $work = 'Coolblue NL';
+ echo 'Voornaam: ' . $firstName . '<br>' . 'Achternaam: ' . $lastName . '<br>' . 'Geboortedatum: ' . $dateOfbirth . 
+ '<br>' . 'Leeftijd: '. $age . '<br>' . 'Geboorteplaats: ' . $birthPlace . '<br>' . 'Email: ' . $email . '<br>' . 
+ 'Telefoonnummer: ' . $mobileNumber . '<br>' . "Hobby's: " . $hobbies . '<br>' . 'Favoriete maaltijd: ' . 
+ $favoriteFood . '<br>' . 'Werk: ' . $work;
+ ?>
